ffa53f8f8ead1a9b83927f0baa53ee9bac7e11fb
Anthony Dixon <adixon88@sky.com>
2022-04-25 13:56:34 +0100

    Script now creates div element with id

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // Script
+
+function createDiv() {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("id", "backgroundDiv");
+    document.body.append(newDiv);
+}
+
+createDiv();
+
